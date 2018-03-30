64916d0a6157b0fc641e4432df8f0ee60c2d691c
Sean McShane <svsean416@yahoo.com>
2018-03-30 03:13:12 +0000

    Started init version of artist detail page on website

--- src/nodejs/website/server.js
+++ src/nodejs/website/server.js
@@ -39,15 +39,28 @@ connectToDatabase(function(dbconn){
 })
 
 app.get('/', function (req, res) {
-    fs.readFile("html/index.html", "utf8", function(err, html){
-        if(err)
-            console.log(err)
-        else{
-            fillTable(html, typeof req.query.start === "undefined" ? 1 : req.query.start, 25, function(modHTML){
-                res.send(modHTML);
-            });
-        }
-    });
+   if(typeof req.query.artist !== "undefined"){
+        fs.readFile("html/artist.html", "utf8", function(err, html){
+            if(err)
+                console.log(err)
+            else{
+                modifyArtistPage(html, req.query.artist, function(modHTML){
+                    res.send(modHTML);
+                });
+            }
+        });
+    }
+    else{
+        fs.readFile("html/index.html", "utf8", function(err, html){
+            if(err)
+                console.log(err);
+            else{
+                modifyRankingsPage(html, typeof req.query.start === "undefined" ? 1 : req.query.start, 25, function(modHTML){
+                    res.send(modHTML);
+                });
+            }
+        });
+    }
 })
 
 app.get('/css/main.css', function (req, res) {
@@ -58,7 +71,7 @@ app.get('/js/main.js', function (req, res) {
     res.sendFile("js/main.js", {root: __dirname });
 })
 
-function fillTable(html, startIdx, resultSize, callback){
+function modifyRankingsPage(html, startIdx, resultSize, callback){
     conn.query("select distinct(b.name), a.score, c.genre, b.most_recent_release_date from (select * from artist_metrics limit " + resultSize + " offset " + 
                     (startIdx-1) + ") as a join artist_info as b on a.id = b.id left join genres as c on b.id = c.id;", function(err, result, fields) {
         if(err)
@@ -76,7 +89,7 @@ function fillTable(html, startIdx, resultSize, callback){
                 result[i-1].most_recent_release_date = (!result[i-1].most_recent_release_date ? "n/a" : result[i-1].most_recent_release_date.toString().substring(4, 15)); 
                 tableString +=  "<tr>" +
                                     "<td>" + (i - 1 + (+startIdx)) + "</td>" +
-                                    "<td>" + result[i-1].name + "</td>" +
+                                    "<td><a class='artistLink' href='/?artist=" + result[i-1].name + "'>" + result[i-1].name + "</a></td>" +
                                     "<td>" + result[i-1].genre + "</td>" +
                                     "<td>" + result[i-1].score + "</td>" +
                                     "<td>" + result[i-1].most_recent_release_date + "</td>" +
@@ -90,6 +103,23 @@ function fillTable(html, startIdx, resultSize, callback){
             $("#pageInfo").each(function(){
                 $(this).html("Showing results <span id='low'>" + startIdx + "</span> - <span id='high'>" + (+startIdx + +resultSize - 1) + "</span>");
             }); 
+            callback(dom.window.document.documentElement.outerHTML);
+        }
+    });
+}
+
+function modifyArtistPage(html, artist, callback){
+    conn.query("select * from (select * from artist_info where name='" + artist + "' order by most_recent_release_date desc limit 1) as a " + 
+                        "join artist_metrics as b on a.id=b.id left join genres as c on b.id=c.id;", function(err, result,fields) {
+        if(err)
+            console.log(err);
+        else{
+            var dom = new JSDOM(html);
+            var $ = require('jquery')(dom.window);
+            $(".bodyContainer").each(function () {
+                $(this).html("<img id='artistImage' src='" + result[0].image_url + "'>");
+            });
+
             callback(dom.window.document.documentElement.outerHTML);
         }
     });
--- src/r/test.r
+++ src/r/test.r
@@ -4,7 +4,7 @@ library(dplyr)
 conn = dbConnect(MySQL(), user="node", password="nodejs", 
                     dbname="spotify_artist_finder_db", host="localhost")
 
-rs = dbSendQuery(conn, "select id from artist_metrics where date between '2018-03-26 00:00:00' and '2018-03-26 23:59:59' group by id having count(*) > 1;")
+rs = dbSendQuery(conn, "select id from artist_metrics where date between '2018-03-29 00:00:00' and '2018-03-29 23:59:59' group by id having count(*) > 1;")
 data = fetch(rs, n = -1)
 
 apply(data, 1, function(row){
